df771a8935d2b75697b6020a46ffa90833867a4e
Pedro F Marquez <pfernandom@gmail.com>
2023-10-26 16:31:57 -0500

    Fix Jest project references

--- jest.config.js
+++ jest.config.js
@@ -3,26 +3,5 @@ module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
     rootDir: "./",
-    projects: [
-        {
-            displayName: "Core",
-            testEnvironment: "node",
-            moduleNameMapper: {
-                "^@tests/(.*)$": "<rootDir>/packages/core/tests/$1",
-                "^@/(.*)$": "<rootDir>/packages/core/src/$1"
-            },
-            transform: {
-                "^.+\\.tsx?$": [
-                    "ts-jest",
-                    { tsconfig: "<rootDir>/packages/core/tsconfig.json" }
-                ]
-            },
-            testMatch: [
-                "<rootDir>/packages/core/tests/unit/**/*.test.ts",
-                "<rootDir>/packages/core/tests/integration/**/*.test.ts",
-                "<rootDir>/packages/core/tests/unit/**/*.test.tsx",
-                "<rootDir>/packages/core/tests/integration/**/*.test.tsx"
-            ]
-        }
-    ]
+    projects: ["packages/core"]
 };
